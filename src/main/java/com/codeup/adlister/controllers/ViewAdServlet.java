a998978513f0c09ca012a6f8bcf7787adc120669
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-02-20 09:17:10 -0600

    add ViewAdServlet

---
+++ src/main/java/com/codeup/adlister/controllers/ViewAdServlet.java
@@ -0,0 +1,32 @@
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.models.Ad;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import static com.codeup.adlister.dao.DaoFactory.getAdsDao;
+
+@WebServlet(name = "controllers.ViewAdServlet", urlPatterns = "/ads/show")
+public class ViewAdServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getParameter("id")==null){
+            response.sendRedirect("/ads");
+        }
+        long id = Long.parseLong(request.getParameter("id"));
+        System.out.println(id);
+        try {
+            Ad oneAd = getAdsDao().singleAd(id);
+            System.out.println(oneAd.getTitle());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        request.getRequestDispatcher("/WEB-INF/ads/adPage.jsp").forward(request, response);
+    }
+}
