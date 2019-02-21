491ab485323557ba46e5299f5f78a58a71af3437
AaronBoehle <boehle.aaron@gmail.com>
2019-02-21 16:11:54 -0600

    refactored code on RegisterServlet

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -3,6 +3,7 @@ package com.codeup.adlister.controllers;
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
 import com.codeup.adlister.util.Password;
+import jdk.nashorn.internal.ir.RuntimeNode;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -25,36 +26,27 @@ public class RegisterServlet extends HttpServlet {
         String password = request.getParameter("password");
         String passwordConfirmation = request.getParameter("confirm_password");
         String hashPassword = Password.hash(password);
+        boolean userAlreadyExists = DaoFactory.getUsersDao().isAlreadyRegisteredUsername(username);
+        boolean emailAlreadyRegistered = DaoFactory.getUsersDao().isAlreadyRegisteredEmail(email);
+        boolean passwordsMatch = password.equals(passwordConfirmation);
 
-
-        // displays individual error messages if inputHasErrors
-        // if statements are independent from each other so each message can be displayed per error type
-        if (DaoFactory.getUsersDao().isAlreadyRegisteredUsername(username)) {
-            request.setAttribute("errorMessageUsername", "Username \'" + username + "\' is already taken.");
-        }
-        if (DaoFactory.getUsersDao().isAlreadyRegisteredEmail(email)) {
-            request.setAttribute("errorMessageEmail", "Email address \'" + email + "\' is already registered with an active account.");
-        }
-        if ((!password.equals(passwordConfirmation))) {
-                    request.setAttribute("errorMessagePassword", "Passwords do not match.");
-        }
+        // "if" statements are independent from one another so that each error message displays individually
+        if (userAlreadyExists) {request.setAttribute("errorMessageUsername", "Username \'" + username + "\' is already taken.");}
+        if (emailAlreadyRegistered) {request.setAttribute("errorMessageEmail", "Email address \'" + email + "\' is already registered with an active account.");}
+        if (!passwordsMatch) {request.setAttribute("errorMessagePassword", "Passwords do not match.");}
 
         // validate input
-        boolean inputHasErrors = DaoFactory.getUsersDao().isAlreadyRegisteredUsername(username)
-                || DaoFactory.getUsersDao().isAlreadyRegisteredEmail(email)
-                || (!password.equals(passwordConfirmation));
+        boolean inputHasErrors = userAlreadyExists || emailAlreadyRegistered || !passwordsMatch;
 
         if (inputHasErrors) {
             request.setAttribute("stickyUser", username);
             request.setAttribute("stickyEmail", email);
             request.getSession().setAttribute("inputHasErrors", true);
             request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
-        }
-        else {
+        } else {
             User user = new User(username, email, hashPassword);
             DaoFactory.getUsersDao().insert(user);
             request.getSession().setAttribute("inputHasErrors", false);
-            request.removeAttribute("validAttempt");
             request.getSession().setAttribute("loginMessage", "Registration Successful! Please login to your new account.");
             request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
         }
